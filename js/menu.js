96617af3258e3462b1f8cc8d5f35f84d2e72fc49
koleso8 <dev.koleso@gmail.com>
2024-05-31 19:16:40 +0300

    add

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-const menuBtns = document.querySelectorAll('.js-menu-btn ')
+const menuBtns = document.querySelectorAll('.js-menu-btn')
 const menuEl = document.querySelector('.menu-container')
 
 menuBtns.forEach((btn) => {
